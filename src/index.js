ff2b4fdaa0bf28d59a75f3034285c8970937a51b
Daniel Sato <dksato@gmail.com>
2018-04-09 16:24:42 -0500

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
 document.title = 'BookBrunch';
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
